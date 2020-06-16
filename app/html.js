89776c8fecdb6e58771df5d6fff4bb029b733452
Grigore <grigore.mereacre95@gmail.com>
2020-06-16 18:47:06 -0500

    templates added

--- app/html.js
+++ app/html.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const templates = path.resolve(name, "../html");
+const templatesDir = path.resolve(dirname, "../templates");
 
 const define = employees => {
     const html = [];
@@ -12,4 +12,46 @@ const define = employees => {
     html.push(employees.filter(employee => employee.getStatus() === "Intern")
     .map(intern => defineIntern(intern)));
     return defineMain(html.join(""));
-}
+};
+//
+const defineManager = manager => {
+    let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"),"utf8");
+    template = replacePlaceholders(template, "name", manager.getName());
+    template = replacePlaceholders(template, "status", manager.getStatus());
+    template = replacePlaceholders(template, "email", manager.getEmail());
+    template = replacePlaceholders(template, "officenumber", manager.getOfficeNumber());
+    template = replacePlaceholders(template, "id", manager.getId());
+    return template;
+};
+//
+const defineEngineer = engineer => {
+    let template = fs.readFileSync(path.resolve(templatesDir, "engineer.html"),"utf8");
+    template = replacePlaceholders(template, "name", engineer.getName());
+    template = replacePlaceholders(template, "status", engineer.getStatus());
+    template = replacePlaceholders(template, "email", engineer.getEmail());
+    template = replacePlaceholders(template, "github", engineer.getGithub());
+    template = replacePlaceholders(template, "id", engineer.getId());
+    return template;
+};
+//
+const defineIntern = intern => {
+    let template = fs.readFileSync(path.resolve(templatesDir, "intern.html"),"utf8");
+    template = replacePlaceholders(template, "name", intern.getName());
+    template = replacePlaceholders(template, "status", intern.getStatus());
+    template = replacePlaceholders(template, "email", intern.getEmail());
+    template = replacePlaceholders(template, "school", intern.getSchool());
+    template = replacePlaceholders(template, "id", intern.getId());
+    return template;
+};
+
+const defineMain = html => {
+    const template = fs.readFileSync(path.resolve(templatesDir, "main.html"), "utf8");
+    return replacePlaceholders(template, "team", html);
+  };
+  
+  const replacePlaceholders = (template, placeholder, value) => {
+    const pattern = new RegExp("{{ " + placeholder + " }}");
+    return template.replace(pattern, value);
+  };
+  
+  module.exports = render;
